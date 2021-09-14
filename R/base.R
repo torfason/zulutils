e51618953e5ead8d9415b213cd0e77b695b3b01a
Magnus Thor Torfason <m@zulutime.net>
2021-09-14 11:31:18 +0000

    Update base.R to use @md in docs, and other docs improvements

--- R/base.R
+++ R/base.R
@@ -8,12 +8,13 @@
 
 #' Return a single argument unchanged
 #'
-#' It can be useful (when developing pipelines for example),
+#' It can often be useful (when developing pipelines for example),
 #' to have access to a function that simply returns the element
 #' that it receives. This is that function.
 #'
-#' @param x The single argument to (not) process
-#' @return The argument \code{x} is returned unchanged
+#' @param x The single argument to (not) process.
+#' @return The argument `x` is returned unchanged.
+#' @md
 #' @export
 noop = function(x)
 {
@@ -30,9 +31,10 @@ noop = function(x)
 #' an empty sequence, if the difference is greater, the function will
 #' error out.
 #'
-#' @param from The lower bound of the sequence
-#' @param to   The higher bound of the sequence
-#' @return     A sequence ranging from \code{from} to \code{to}
+#' @param from The lower bound of the sequence.
+#' @param to   The higher bound of the sequence.
+#' @return A sequence ranging from `from` to `to`.
+#' @md
 #' @export
 zeq = function(from, to)
 {
@@ -45,15 +47,19 @@ zeq = function(from, to)
 
 #' Sample from a vector or data.frame in a safe way
 #'
-#' This function duplicates the functionality of sample(), with the exception
+#' This function duplicates the functionality of [base::sample()], with the exception
 #' that it does not attempt the (rather silly in my opinion) user-friendliness
 #' of switching the interpretation of the first element to a number if the
 #' length of the vector is 1.  To sample from an interval between 1 and n,
-#' simply use zample(1:n) (or use sample(n) instead)
+#' simply use zample(1:n) (or use sample(n) instead).
 #'
-#' @param x       The vector to sample from
-#' @param size    The number of elements to sample from \code{x} (defaults to \code{length(x)})
-#' @param replace Should elements be replaced after sampling (defaults to \code{false})
+#' @param x       The vector to sample from.
+#' @param size    The number of elements to sample from `x`. (defaults to `length(x)`)
+#' @param replace Should elements be replaced after sampling. (defaults to `FALSE`)
+#' @param prob    Vector specifying the probabilities. (defaults to `NULL` for equal probailities)
+#' @return A sample of elements from the vector `x`.
+#' @seealso [base::sample()]
+#' @md
 #' @export
 zample = function (x, size=length(x), replace = FALSE, prob = NULL)
 {
@@ -83,9 +89,15 @@ zample = function (x, size=length(x), replace = FALSE, prob = NULL)
 #' all values in a given group should be identical, but
 #' you want to make sure.
 #'
-#' Optionally takes a na.rm parameter, similarly to sum, mean and
-#' other aggregate functions.
+#' Optionally takes a `na.rm` parameter, similarly to `sum`, `mean` and
+#' other aggregate functions. If `na.rm` is `TRUE`, a vector with a single
+#' unique value but some `NA` values mixed in is OK, because the `NAs` will
+#' be removed before checks are performed.
 #'
+#' @param x     Vector which chould only include identical values.
+#' @param na.rm Will NAs be removed before checking. (defaults to FALSE)
+#' @return The single unique element in `x`, or error if all elements are not identical.
+#' @md
 #' @export
 zingle = function(x, na.rm = FALSE)
 {
@@ -98,12 +110,12 @@ zingle = function(x, na.rm = FALSE)
 #'
 #' @usage na_replace(x, replace)
 #'
-#' @param x Vector possibly contining missing (NA) values.
-#' @param replace Scalar replacement value
-#' @return Copy of x with any missing values replaced with replace
+#' @param x Vector possibly contining missing (`NA`) values.
+#' @param replace Scalar replacement value.
+#' @return Copy of x with any missing values replaced with replace.
 #'
 #' @family na_functions
-#'
+#' @md
 #' @export
 na_replace = function (x, replace)
 {
@@ -112,21 +124,33 @@ na_replace = function (x, replace)
 }
 
 #' @rdname zulutils-deprecated
-#' @section \code{na.replace}:
-#' \code{na.replace()} is deprecated in favor of \code{\link{na_replace()}}.
+#' @section `na.replace`:
+#' `na.replace()` is deprecated in favor of [na_replace()].
 #'
+#' @md
 #' @export
 na.replace <- function(x, replace) {
   .Deprecated("na_replace", msg="na.replace() is deprecated in favor of na_replace().")
   na_replace(x, replace)
 }
 
-#' Replace values in x that are contained in y with NA
+#' Replace values in x that are contained in y with NA.
 #'
-#' This function is a variant/extension of dplyr::na_if()
+#' @description
+#' This function is a variant/extension of [dplyr::na_if()].
+#' Whereas that version requires `y` to be of length one,
+#' this function allows `y` to be a vector and replaces any
+#' elements of `x` that are found in `y` with `NA` values.
+#'
+#' This function can be very useful when cleaning data and
+#' setting multiple any annoying values to NA.
+#'
+#' @param x Vector to modify.
+#' @param y Value to replace with `NA`.
+#' @return A modified version of `x` that replaces any values that are found in `y` with `NA`.
 #'
 #' @family na_functions
-#'
+#' @md
 #' @export
 na_if_in <- function(x,y)
 {
@@ -138,13 +162,14 @@ na_if_in <- function(x,y)
 #'
 #' Left or right pad a string with space or other
 #' characters in order to get a string of a certain length.
-#' This function is a thin wrapper around stringr::str_pad()
+#' This function is a thin wrapper around [stringr::str_pad()].
 #'
-#' @param string Character vector to pad
-#' @param width How many characters at minimum in the output
-#' @param pad The string to pad with
+#' @param string Character vector to pad.
+#' @param width How many characters at minimum in the output.
+#' @param pad The string to pad with.
 #' @rdname padding
 #' @name   padding
+#' @md
 #' @export
 lpad = function(string, width, pad=" ")
 {
@@ -153,6 +178,7 @@ lpad = function(string, width, pad=" ")
 
 #' @rdname padding
 #' @name   padding
+#' @md
 #' @export
 rpad = function(string, width, pad=" ")
 {
@@ -165,10 +191,13 @@ rpad = function(string, width, pad=" ")
 #' of string, returning TRUE for each element that contains
 #' pattern, and FALSE for the other elements. The argument
 #' order follows the conventions of the stringr package,
-#' and the underlying matching is done using stringr::str_extract()
+#' and the underlying matching is done using [stringr::str_extract()]
 #'
-#' @param string The (possibly vectorized) string to process
-#' @param pattern The (possibly vectorized) regexp pattern to use
+#' @param string The (possibly vectorized) string to process.
+#' @param pattern The (possibly vectorized) regexp pattern to use.
+#' @return A vector with boolean values indicating in which elements
+#'         of `string` matched the corresponding elements in `pattern`.
+#' @md
 #' @export
 bgrep = function(string, pattern)
 {
@@ -185,8 +214,8 @@ bgrep = function(string, pattern)
 #' Semi-proportionality is achieved with the D'Hondt method, which is
 #' a commonly used approach in electoral allocation.
 #'
-#' Other implementations of D'Hondt allocation in \code{R} can be found
-#' in \code{electoral::seats_ha()} and \code{coalitions::dHondt()}. The
+#' Other implementations of D'Hondt allocation in `R` can be found
+#' in [electoral::seats_ha()] and [coalitions::dHondt()]. The
 #' motivation behind this implementation is to have a simple interface
 #' with reasonable defaults, suitable for use in pipes operating on
 #' data in data.frames or tibbles. Party names are also optional,
@@ -204,24 +233,25 @@ bgrep = function(string, pattern)
 #'
 #' @param ties A character variable specifying what to do in case of
 #'   ties. The D'Hondt method does not specify any tie handling, so
-#'   two options are allowed. Specifying \code{"error"} (the default)
+#'   two options are allowed. Specifying `"error"` (the default)
 #'   results in an error if there is a tie for the marginal allocated
-#'   seat. Specifying \code{"first"} means that the party that comes
-#'   first (in the \code{votes} vector) wins. Methods such as
+#'   seat. Specifying `"first"` means that the party that comes
+#'   first (in the `votes` vector) wins. Methods such as
 #'   allocating randomly or favoring the largest party can be achived
-#'   by ordering the \code{votes} vector prior to calling the function.
+#'   by ordering the `votes` vector prior to calling the function.
 #'
 #' @return An integer vector with allocations of seats to parties, in
-#'   the same order as the parties were specified in the \code{votes}
+#'   the same order as the parties were specified in the `votes`
 #'   vector. The length of the result vector always equals the length
-#'   of the \code{votes} vector. The sum of the result vector always
-#'   equals the value in \code{seats}. If the \code{votes} vector
+#'   of the `votes` vector. The sum of the result vector always
+#'   equals the value in `seats`. If the `votes` vector
 #'   has names, the results vector has the same names.
 #'
 #' @examples
 #' dhondt(c(13,11,7,5,3))
 #' dhondt(c(liberals=4876, conservatives=5212, greens=2349), seats=21)
 #'
+#' @md
 #' @export
 dhondt <- function(votes, seats=7, ties=c("error","first")) {
 
